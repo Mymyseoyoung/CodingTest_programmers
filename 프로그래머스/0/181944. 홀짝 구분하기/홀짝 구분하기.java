19670ce250689dfcb0226969c4d2f02b5be688d1
Seoyoung Park <162325695+Mymyseoyoung@users.noreply.github.com>
2026-05-18 22:31:37 +0900

    [level 0] Title: 홀짝 구분하기, Time: 96.31 ms, Memory: 52.8 MB -BaekjoonHub

--- 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.java
+++ 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.java
@@ -4,17 +4,18 @@ import java.io.IOException;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
         int n = Integer.parseInt(br.readLine());
         
-        if(n%2==0){
-            System.out.println(n+" is even");
-            }
-        else 
+        if(n%2==0)
         {
-                   System.out.println(n+" is odd");
-            
+            System.out.println(n + " is even");
         }
+        else
+        {
             
+            System.out.println(n + " is odd");
+        }
     }
 }
