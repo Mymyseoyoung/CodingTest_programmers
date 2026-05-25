ce9693d4a8f20691e6499b0abaddbdf498125a28
Seoyoung Park <162325695+Mymyseoyoung@users.noreply.github.com>
2026-05-25 22:27:02 +0900

    [level 0] Title: 문자열 곱하기, Time: 0.06 ms, Memory: 61.1 MB -BaekjoonHub

--- 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.java
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.java
@@ -1,9 +1,9 @@
 class Solution {
     public String solution(String my_string, int k) {
     
-        StringBuilder sb = new StringBuilder();
+        StringBuilder sb =new StringBuilder();
         
-        for(int i =0; i<k;i++)
+        for(int i=0;i<k;i++)
         {
             sb.append(my_string);
         }
