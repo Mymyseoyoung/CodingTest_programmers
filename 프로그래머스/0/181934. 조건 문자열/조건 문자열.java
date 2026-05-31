82de7b6a1a0e077b3274ecc84870ba46f79030eb
Seoyoung Park <162325695+Mymyseoyoung@users.noreply.github.com>
2026-05-31 22:11:30 +0900

    [level 0] Title: 조건 문자열, Time: 1.16 ms, Memory: 60.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(String ineq, String eq, int n, int m) {
+        
+        String op=ineq+eq;
+        
+        switch(op)
+        {
+            case ">=":
+                 return n >= m ? 1 : 0;
+            case "<=":
+                return n<=m?1:0;
+            case ">!":
+                return n>m?1:0;
+            case "<!":
+                return n<m?1:0;
+        }
+        
+     
+        return 0;
+    }
+}
