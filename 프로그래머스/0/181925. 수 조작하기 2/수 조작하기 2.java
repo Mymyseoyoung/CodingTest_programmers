ee9f42c822af40d09f3dc9c8c8239f8c4245ef23
Seoyoung Park <162325695+Mymyseoyoung@users.noreply.github.com>
2026-07-02 18:51:18 +0900

    [level 0] Title: 수 조작하기 2, Time: 3.59 ms, Memory: 85.7 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -1,36 +1,6 @@
 class Solution {
     public String solution(int[] numLog) {
-        
-        StringBuilder sb=new StringBuilder();
-        
-        //1번 인덱스부터 끝까지 쭉 순회 
-        for(int i=1;i<numLog.length;i++)
-        {
-            int diff=numLog[i]-numLog[i-1];
-            
-            //차이가 몇이냐에 따라 다른 애를 sb에 append 해줌
-            switch(diff)
-            {
-                case 1:
-                    sb.append('w');
-                    break;
-                    
-                case -1:
-                    sb.append('s');
-                    break;
-                    
-                case 10:
-                    sb.append('d');
-                    break;
-                    
-                case -10:
-                    sb.append('a');
-                    break;
-            }
-        }
-        
-        
-        return sb.toString();
+        String answer = "";
+        return answer;
     }
-    
 }
