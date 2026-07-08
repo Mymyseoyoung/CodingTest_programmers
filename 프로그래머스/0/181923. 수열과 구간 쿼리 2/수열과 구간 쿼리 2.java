8846eeca3d033f7abd7ca77b0ee0d3fe9fc0ecf4
Seoyoung Park <162325695+Mymyseoyoung@users.noreply.github.com>
2026-07-08 21:31:45 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 2.13 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
@@ -1,37 +1,34 @@
 class Solution {
     public int[] solution(int[] arr, int[][] queries) {
-        
+    
+        //정답 배열
         int[] result = new int[queries.length];
         
-        for ( int q=0;q<queries.length;q++)
+        for(int i =0; i<queries.length;i++)
         {
-            int s=queries[q][0];
-                
-            int e=queries[q][1];
-                
-            int k=queries[q][2];
+            int s=queries[i][0];
+            int e=queries[i][1];
+            int k=queries[i][2];
             
+            int minVal=Integer.MAX_VALUE;
             
-            //가장 작은 값 
-            int minVal = Integer.MAX_VALUE;
-            
-            for(int i=s;i<=e;i++)
+            for(int q=s;q<=e;q++)
             {
-                if(arr[i]>k&&arr[i]<minVal)
+                if(arr[q]>k&&arr[q]<minVal)
                 {
-                    minVal=arr[i];
+                    minVal=arr[q];
                 }
             }
             
             if(minVal==Integer.MAX_VALUE)
             {
-                result[q]=-1;
-                }
-            else
-            {
-                result[q]=minVal;
+                result[i]=-1;
+            }
+            else{
+                result[i]=minVal;
             }
         }
+        
         return result;
     }
 }
