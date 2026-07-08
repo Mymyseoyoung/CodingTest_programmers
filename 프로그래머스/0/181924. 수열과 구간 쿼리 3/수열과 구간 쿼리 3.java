d889e79848bd08c6933042eb3e06e06944bc86aa
Seoyoung Park <162325695+Mymyseoyoung@users.noreply.github.com>
2026-07-08 17:24:38 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.07 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
@@ -1,11 +1,15 @@
 class Solution {
     public int[] solution(int[] arr, int[][] queries) {
-        for (int[] query : queries) {
+        
+        //queries 배열을 순회하면서 하나씩 !! 
+        for(int[] query : queries)
+        {
             int i = query[0];
-            int j = query[1];
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
+            int j= query[1];
+             int temp = arr[i];
+               arr[i]=arr[j];
+                arr[j]=temp;
+                
         }
         return arr;
     }
