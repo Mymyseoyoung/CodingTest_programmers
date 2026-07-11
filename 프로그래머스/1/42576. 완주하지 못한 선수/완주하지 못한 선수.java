76e767c7651698d0c09c4aa450daf6082de1eebf
Seoyoung Park <162325695+Mymyseoyoung@users.noreply.github.com>
2026-07-11 18:29:50 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 56.47 ms, Memory: 114 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        
+        Map<String, Integer> map = new HashMap<>();
+        
+        //for -each 문을 쓰자 ! 
+        for(String name : participant)
+        {
+            map.put(name,map.getOrDefault(name,0)+1);
+        }
+        for(String name : completion)
+        {
+            map.put(name,map.getOrDefault(name,0)-1);
+        }
+        
+        for(String name : map.keySet())
+        {
+            if(map.get(name) != 0)
+            {
+                return name;
+            }
+        }
+        
+        
+        
+        return null;
+    }
+}
