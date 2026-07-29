4e99402b3c9bd534e3e82885f02f7f6757cd0582
Seoyoung Park <162325695+Mymyseoyoung@users.noreply.github.com>
2026-07-30 00:19:42 +0900

    [level 0] Title: 배열 만들기 5, Time: 5.80 ms, Memory: 93 MB -BaekjoonHub

--- 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.java
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.java
@@ -1,23 +1,27 @@
-import java.util.ArrayList;
-import java.util.List;
-
+import java.util.*;
 class Solution {
     public int[] solution(String[] intStrs, int k, int s, int l) {
+        
         List<Integer> list = new ArrayList<>();
         
-        for (String str : intStrs) {
-            String sub = str.substring(s, s + l);
+        for (String str : intStrs)
+        {
+            String sub = str.substring(s,s+l);
             int value = Integer.parseInt(sub);
-            if (value > k) {
+            
+            if(value>k)
+            {
                 list.add(value);
             }
         }
         
         int[] answer = new int[list.size()];
-        for (int i = 0; i < answer.length; i++) {
-            answer[i] = list.get(i);
+        
+        for(int i =0;i<answer.length;i++)
+        {
+            answer[i]=list.get(i);
         }
         
-        return answer;
+    return answer;
     }
 }
